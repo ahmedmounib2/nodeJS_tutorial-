42d3cb4b3041f9c4041c6c370e9a68cbd44e83f2
ahmedmounib2 <ahmedmounib2@gmail.com>
2024-07-29 04:19:29 +0300

    MVC REST API

--- 06/routes/root.js
+++ 06/routes/root.js
@@ -7,12 +7,5 @@ router.get('^/$|/index.(html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-router.get('/new-page.(html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
-});
-
-router.get('/old-page.(html)?', (req, res) => {
-    res.redirect(301, '/new-page.html');  // 02 by default , set 301 for permanent redirect
-});
 
 module.exports = router;
